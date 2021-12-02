34f7b345a282d3aa929baed3fbee67f8237e628d
13773753970 <32631243+13773753970@users.noreply.github.com>
2021-12-02 23:22:55 +0800

    add log middleware

--- Sources/Redux/LogMiddleware.swift
+++ Sources/Redux/LogMiddleware.swift
@@ -7,16 +7,18 @@
 
 import Foundation
 
-public typealias Log = (String, Any) -> Void
+public typealias Log = (String, Any?) -> Void
 
 public func createLogMiddleware<State, Action>(_ log: @escaping Log) -> Enhancer<State, Action> {
     func middleware(_ getState: @escaping GetState<State>, _ dispatch: @escaping Dispatch<Action>) -> (@escaping Dispatch<Action>) -> Dispatch<Action> {
         return { originalDispatch in
             return { action in
-                log("state:", getState())
-                log("action:", type(of: action))
+                log("------------------------------------------------------", nil)
+                log("prevState", getState())
+                log("action", type(of: action as Any))
                 originalDispatch(action)
-                log("state:", getState())
+                log("afterState", getState())
+                log("------------------------------------------------------", nil)
             }
         }
     }
